0f8f9167b6795d14d238474b9e90c40f1f0b61c9
John Flavin <flavinj@gmail.com>
2016-02-24 19:30:47 -0600

    Removed exception-handling TODO from ContainersConfig

--- src/main/java/org/nrg/containers/config/ContainersConfig.java
+++ src/main/java/org/nrg/containers/config/ContainersConfig.java
@@ -1,38 +1,8 @@
 package org.nrg.containers.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("org.nrg.containers.rest, " +
-        "org.nrg.containers.services, " +
-        "org.nrg.containers.services.impl")
-public class ContainersConfig {
-
-// TODO Add Exception handling
-//    @Bean
-//    public SimpleMappingExceptionResolver exceptionResolver() {
-//        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
-//
-//        Properties exceptionMappings = new Properties();
-//
-//        exceptionMappings.put("net.petrikainulainen.spring.testmvc.todo.exception.TodoNotFoundException", "error/404");
-//        exceptionMappings.put("java.lang.Exception", "error/error");
-//        exceptionMappings.put("java.lang.RuntimeException", "error/error");
-//
-//        exceptionResolver.setExceptionMappings(exceptionMappings);
-//
-//        Properties statusCodes = new Properties();
-//
-//        statusCodes.put("error/404", "404");
-//        statusCodes.put("error/error", "500");
-//
-//        exceptionResolver.setStatusCodes(statusCodes);
-//
-//        return exceptionResolver;
-//    }
-
-    private static final Logger _log = LoggerFactory.getLogger(ContainersConfig.class);
-}
+@ComponentScan("org.nrg.containers.rest, org.nrg.containers.services")
+public class ContainersConfig {}
