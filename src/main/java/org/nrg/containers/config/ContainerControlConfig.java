14a0013709e02f64152243f15006f8d6d03285a7
John Flavin <flavinj@gmail.com>
2016-02-26 09:07:04 -0600

    I think I only need one Config. And maybe two would break things? One. One is good.

--- src/main/java/org/nrg/containers/config/ContainerControlConfig.java
+++
@@ -1,8 +0,0 @@
-package org.nrg.containers.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.nrg.containers.api, org.nrg.containers.services")
-public class ContainerControlConfig {}
--- src/main/java/org/nrg/containers/config/ContainerServiceConfig.java
+++ src/main/java/org/nrg/containers/config/ContainerServiceConfig.java
@@ -4,5 +4,5 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("org.nrg.containers.rest, org.nrg.containers.services")
+@ComponentScan("org.nrg.containers.rest, org.nrg.containers.services, org.nrg.containers.api")
 public class ContainerServiceConfig {}
