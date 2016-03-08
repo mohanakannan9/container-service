013e6bc224825229aaf78783b1e6c8c58bbaa8e1
Matt Kelsey <kelseym@wustl.edu>
2016-03-07 20:43:40 -0600

    Refactored DockerControlApi and ContainerControlApi(Interface) to get Docker server properties from Prefs service.

--- src/main/java/org/nrg/containers/api/ContainerControlApi.java
+++ src/main/java/org/nrg/containers/api/ContainerControlApi.java
@@ -1,6 +1,7 @@
 package org.nrg.containers.api;
 
 import org.nrg.containers.exceptions.ContainerServerException;
+import org.nrg.containers.exceptions.NoServerPrefException;
 import org.nrg.containers.exceptions.NotFoundException;
 import org.nrg.containers.model.Container;
 import org.nrg.containers.model.Image;
@@ -9,19 +10,21 @@ import java.util.List;
 import java.util.Map;
 
 public interface ContainerControlApi {
-    List<Image> getAllImages(final String server);
+    String server() throws NoServerPrefException;
 
-    Image getImageByName(final String server, final String imageName) throws ContainerServerException, NotFoundException;
+    List<Image> getAllImages();
 
-    Image getImageById(final String server, final String imageId) throws NotFoundException, ContainerServerException;
+    Image getImageByName(final String imageName) throws ContainerServerException, NotFoundException;
 
-    List<Container> getAllContainers(final String server);
+    Image getImageById(final String imageId) throws NotFoundException, ContainerServerException;
 
-    List<Container> getContainers(final String server, final Map<String, String> params);
+    List<Container> getAllContainers();
 
-    Container getContainer(final String server, final String id) throws NotFoundException;
+    List<Container> getContainers(final Map<String, String> params);
 
-    String getContainerStatus(final String server, final String id) throws NotFoundException;
+    Container getContainer(final String id) throws NotFoundException;
 
-    String launchImage(final String server, final String imageName, final String[] runCommand, final String[] volumes);
+    String getContainerStatus(final String id) throws NotFoundException;
+
+    String launchImage(final String imageName, final String[] runCommand, final String[] volumes);
 }
