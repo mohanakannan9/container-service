c0686b6355e815a2d6355845d1d3f0031ba4df41
John Flavin <flavinj@gmail.com>
2016-03-18 15:33:07 -0500

    Add dependency on transporter. Import its config into containers config.

--- src/main/java/org/nrg/containers/config/ContainersSpringConfig.java
+++ src/main/java/org/nrg/containers/config/ContainersSpringConfig.java
@@ -1,6 +1,7 @@
 package org.nrg.containers.config;
 
 import org.nrg.framework.annotations.XnatModule;
+import org.nrg.transporter.config.TransporterConfig;
 import org.nrg.xnat.configuration.AutomationConfig;
 import org.nrg.xnat.configuration.PreferencesConfig;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,5 +14,5 @@ import org.springframework.context.annotation.Import;
 @XnatModule(value = "container-service", description = "XNAT Container Service", config = ContainersSpringConfig.class)
 @ComponentScan(value = "org.nrg.containers",
         excludeFilters = @Filter(type = FilterType.REGEX, pattern = ".*TestConfig.*", value = {}))
-@Import({PreferencesConfig.class, AutomationConfig.class})
+@Import({PreferencesConfig.class, AutomationConfig.class, TransporterConfig.class})
 public class ContainersSpringConfig {}
