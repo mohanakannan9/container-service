51130005cd980f952043f6238f401494056ffb3a
John Flavin <flavinj@gmail.com>
2016-03-21 20:25:23 -0500

    Update plugin annotation

--- src/main/java/org/nrg/containers/config/ContainersSpringConfig.java
+++ src/main/java/org/nrg/containers/config/ContainersSpringConfig.java
@@ -1,13 +1,13 @@
 package org.nrg.containers.config;
 
-import org.nrg.framework.annotations.XnatModule;
+import org.nrg.framework.annotations.XnatPlugin;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@XnatModule(value = "container-service", description = "XNAT Container Service", config = ContainersSpringConfig.class)
+@XnatPlugin(value = "container-service", description = "XNAT Container Service", config = ContainersSpringConfig.class)
 @ComponentScan(value = "org.nrg.containers",
         excludeFilters = @Filter(type = FilterType.REGEX, pattern = ".*TestConfig.*", value = {}))
 public class ContainersSpringConfig {}
