77891a8dcfd2ab324c30681dd3fcd3d288f8f374
John Flavin <flavinj@gmail.com>
2016-05-31 07:12:15 -0500

    Move resolveCommand to ResolvedCommandService

--- src/main/java/org/nrg/actions/services/CommandService.java
+++ src/main/java/org/nrg/actions/services/CommandService.java
@@ -1,11 +1,8 @@
 package org.nrg.actions.services;
 
-import org.nrg.actions.model.ActionContextExecution;
 import org.nrg.actions.model.Command;
-import org.nrg.actions.model.ResolvedCommand;
-import org.nrg.containers.exceptions.NotFoundException;
 import org.nrg.framework.orm.hibernate.BaseHibernateService;
 
 public interface CommandService extends BaseHibernateService<Command> {
-    ResolvedCommand resolveCommand(ActionContextExecution ace) throws NotFoundException;
+
 }
