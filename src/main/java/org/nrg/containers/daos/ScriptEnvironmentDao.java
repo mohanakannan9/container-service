51a2ba4d5f06f21c3d9b5450bc4491ff55d6157a
John Flavin <flavinj@gmail.com>
2016-10-14 12:18:27 -0500

    Remove unused classes

--- src/main/java/org/nrg/containers/daos/ScriptEnvironmentDao.java
+++
@@ -1,9 +0,0 @@
-package org.nrg.containers.daos;
-
-import org.nrg.containers.model.ScriptEnvironment;
-import org.nrg.framework.orm.hibernate.AbstractHibernateDAO;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ScriptEnvironmentDao extends AbstractHibernateDAO<ScriptEnvironment> {
-}
