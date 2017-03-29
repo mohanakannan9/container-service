7543b69872a2a6753183283e74e590b10cfde4a3
John Flavin <flavinj@gmail.com>
2017-03-29 10:44:36 -0500

    DockerImage: Immutable*, Builder

--- src/main/java/org/nrg/containers/model/image/docker/DockerImage.java
+++ src/main/java/org/nrg/containers/model/image/docker/DockerImage.java
@@ -3,12 +3,15 @@ package org.nrg.containers.model.image.docker;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import io.swagger.annotations.ApiModel;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -17,22 +20,46 @@ import java.util.Map;
 public abstract class DockerImage {
 
     @Nullable @JsonProperty("image-id") public abstract String imageId();
-    @JsonProperty("tags") public abstract List<String> tags();
-    @JsonProperty("labels") public abstract Map<String, String> labels();
+    @JsonProperty("tags") public abstract ImmutableList<String> tags();
+    @JsonProperty("labels") public abstract ImmutableMap<String, String> labels();
 
     @JsonCreator
     public static DockerImage create(@JsonProperty("image-id") final String imageId,
                                      @JsonProperty("tags") final List<String> repoTags,
                                      @JsonProperty("labels") final Map<String, String> labels) {
-        return new AutoValue_DockerImage(imageId,
-                repoTags == null ? Lists.<String>newArrayList() : repoTags,
-                labels == null ? Maps.<String, String>newHashMap() : labels);
+        return builder()
+                .imageId(imageId)
+                .tags(repoTags == null ? Collections.<String>emptyList() : repoTags)
+                .labels(labels == null ? Collections.<String, String>emptyMap() : labels)
+                .build();
     }
 
-    public void addTag(final String tag) {
-        if (StringUtils.isBlank(tag)) {
-            return;
+    public static Builder builder() {
+        return new AutoValue_DockerImage.Builder();
+    }
+
+    @AutoValue.Builder
+    public static abstract class Builder {
+        public abstract Builder imageId(String imageId);
+
+        public abstract Builder tags(List<String> tags);
+        abstract ImmutableList.Builder<String> tagsBuilder();
+        public Builder addTag(final String tag) {
+            if (StringUtils.isNotBlank(tag)) {
+                tagsBuilder().add(tag);
+            }
+            return this;
         }
-        tags().add(tag);
+
+        public abstract Builder labels(Map<String, String> labels);
+        abstract ImmutableMap.Builder<String, String> labelsBuilder();
+        public Builder addLabel(final String name, final String value) {
+            if (StringUtils.isNotBlank(name)) {
+                labelsBuilder().put(name, value);
+            }
+            return this;
+        }
+
+        public abstract DockerImage build();
     }
 }
