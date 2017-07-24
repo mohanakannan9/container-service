4476adfb7c4dc726219f0c02e639a0334b10a919
John Flavin <flavinj@gmail.com>
2017-07-24 08:56:30 -0500

    Rename Container -> ContainerMessage

--- src/main/java/org/nrg/containers/model/container/auto/ContainerMessage.java
+++ src/main/java/org/nrg/containers/model/container/auto/ContainerMessage.java
@@ -9,13 +9,13 @@ import javax.annotation.Nullable;
 
 @AutoValue
 @JsonInclude(JsonInclude.Include.ALWAYS)
-public abstract class Container {
+public abstract class ContainerMessage {
     @JsonProperty("id") public abstract String id();
     @Nullable @JsonProperty("status") public abstract String status();
 
     @JsonCreator
-    public static Container create(@JsonProperty("id") final String id,
-                                  @JsonProperty("status") final String status) {
-        return new AutoValue_Container(id, status);
+    public static ContainerMessage create(@JsonProperty("id") final String id,
+                                          @JsonProperty("status") final String status) {
+        return new AutoValue_ContainerMessage(id, status);
     }
 }
