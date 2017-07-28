f58522871b303b7b656c4efc16ce25788eeb0e18
John Flavin <flavinj@gmail.com>
2017-07-28 11:58:13 -0500

    Instead of using 0L for "blank ID", use a null string

--- src/main/java/org/nrg/containers/model/command/auto/LaunchReport.java
+++ src/main/java/org/nrg/containers/model/command/auto/LaunchReport.java
@@ -16,8 +16,8 @@ import java.util.Map;
 public abstract class LaunchReport {
     @JsonProperty("status") public abstract String status();
     @JsonProperty("params") public abstract ImmutableMap<String, String> launchParams();
-    @Nullable @JsonProperty("command-id") public abstract Long commandId();
-    @Nullable @JsonProperty("wrapper-id") public abstract Long wrapperId();
+    @Nullable @JsonProperty("command-id") public abstract String commandId();
+    @Nullable @JsonProperty("wrapper-id") public abstract String wrapperId();
     @Nullable @JsonProperty("wrapper-name") public abstract String wrapperName();
 
     @AutoValue
@@ -31,22 +31,36 @@ public abstract class LaunchReport {
         static Success create(@JsonProperty("status") final String ignoredStatus,
                               @JsonProperty("params") final Map<String, String> launchParams,
                               @JsonProperty("container-id") final @Nonnull String containerId,
-                              @JsonProperty("command-id") final Long commandId,
-                              @JsonProperty("wrapper-id") final Long wrapperId,
+                              @JsonProperty("command-id") final String commandId,
+                              @JsonProperty("wrapper-id") final String wrapperId,
                               @JsonProperty("wrapper-name") final String wrapperName) {
             return create(launchParams, containerId, commandId, wrapperId, wrapperName);
         }
 
         public static Success create(final Map<String, String> launchParams,
                                      final @Nonnull String containerId,
-                                     final Long commandId,
-                                     final Long wrapperId,
+                                     final long commandId,
+                                     final long wrapperId,
                                      final String wrapperName) {
             final ImmutableMap<String, String> launchParamsCopy =
                     launchParams == null ?
                             ImmutableMap.<String, String>of() :
                             ImmutableMap.copyOf(launchParams);
-            return new AutoValue_LaunchReport_Success(STATUS, launchParamsCopy, commandId, wrapperId, wrapperName, containerId);
+            final String commandIdString = commandId == 0L ? null : String.valueOf(commandId);
+            final String wrapperIdString = wrapperId == 0L ? null : String.valueOf(wrapperId);
+            return create(launchParamsCopy, containerId, commandIdString, wrapperIdString, wrapperName);
+        }
+
+        public static Success create(final Map<String, String> launchParams,
+                                     final @Nonnull String containerId,
+                                     final String commandIdString,
+                                     final String wrapperIdString,
+                                     final String wrapperName) {
+            final ImmutableMap<String, String> launchParamsCopy =
+                    launchParams == null ?
+                            ImmutableMap.<String, String>of() :
+                            ImmutableMap.copyOf(launchParams);
+            return new AutoValue_LaunchReport_Success(STATUS, launchParamsCopy, commandIdString, wrapperIdString, wrapperName, containerId);
         }
     }
 
@@ -61,22 +75,32 @@ public abstract class LaunchReport {
         static Failure create(@JsonProperty("status") final String ignoredStatus,
                               @JsonProperty("params") final Map<String, String> launchParams,
                               @JsonProperty("message") final @Nonnull String message,
-                              @JsonProperty("command-id") final Long commandId,
-                              @JsonProperty("wrapper-id") final Long wrapperId,
+                              @JsonProperty("command-id") final String commandId,
+                              @JsonProperty("wrapper-id") final String wrapperId,
                               @JsonProperty("wrapper-name") final String wrapperName) {
             return create(launchParams, message, commandId, wrapperId, wrapperName);
         }
 
         public static Failure create(final Map<String, String> launchParams,
                                      final @Nonnull String message,
-                                     final Long commandId,
-                                     final Long wrapperId,
+                                     final long commandId,
+                                     final long wrapperId,
+                                     final String wrapperName) {
+            final String commandIdString = commandId == 0L ? null : String.valueOf(commandId);
+            final String wrapperIdString = wrapperId == 0L ? null : String.valueOf(wrapperId);
+            return create(launchParams, message, commandIdString, wrapperIdString, wrapperName);
+        }
+
+        public static Failure create(final Map<String, String> launchParams,
+                                     final @Nonnull String message,
+                                     final String commandIdString,
+                                     final String wrapperIdString,
                                      final String wrapperName) {
             final ImmutableMap<String, String> launchParamsCopy =
                     launchParams == null ?
                             ImmutableMap.<String, String>of() :
                             ImmutableMap.copyOf(launchParams);
-            return new AutoValue_LaunchReport_Failure(STATUS, launchParamsCopy, commandId, wrapperId, wrapperName, message);
+            return new AutoValue_LaunchReport_Failure(STATUS, launchParamsCopy, commandIdString, wrapperIdString, wrapperName, message);
         }
     }
 
