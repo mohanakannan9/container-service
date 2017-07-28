mohanakannan9/container-service

commit 7b9a9a2853cce6a64afae9eb7484624d3086b413

Rename wrapper input properties to remove 'xnat'

src/main/java/org/nrg/containers/model/container/entity/ContainerEntity.java
@@ -26,7 +26,7 @@ import java.util.Objects;
 @Audited
 public class ContainerEntity extends AbstractHibernateEntity {
     @JsonProperty("command-id") private long commandId;
-    @JsonProperty("xnat-command-wrapper-id") private long xnatCommandWrapperId;
+    @JsonProperty("wrapper-id") private long wrapperId;
     @JsonProperty("docker-image") private String dockerImage;
     @JsonProperty("command-line") private String commandLine;
     @JsonProperty("working-directory") private String workingDirectory;
@@ -48,7 +48,7 @@ public class ContainerEntity extends AbstractHibernateEntity {
         this.userId = userId;
 
         this.commandId = resolvedCommand.commandId();
-        this.xnatCommandWrapperId = resolvedCommand.wrapperId();
+        this.wrapperId = resolvedCommand.wrapperId();
         this.dockerImage = resolvedCommand.image();
         this.commandLine = resolvedCommand.commandLine();
         this.workingDirectory = resolvedCommand.workingDirectory();
@@ -84,7 +84,7 @@ public class ContainerEntity extends AbstractHibernateEntity {
     public ContainerEntity update(final Container containerPojo) {
         this.setId(containerPojo.databaseId());
         this.setCommandId(containerPojo.commandId());
-        this.setXnatCommandWrapperId(containerPojo.wrapperId());
+        this.setWrapperId(containerPojo.wrapperId());
         this.setContainerId(containerPojo.containerId());
         this.setUserId(containerPojo.userId());
         this.setDockerImage(containerPojo.dockerImage());
@@ -136,12 +136,12 @@ public class ContainerEntity extends AbstractHibernateEntity {
         this.commandId = commandId;
     }
 
-    public long getXnatCommandWrapperId() {
-        return xnatCommandWrapperId;
+    public long getWrapperId() {
+        return wrapperId;
     }
 
-    public void setXnatCommandWrapperId(final long xnatCommandWrapperId) {
-        this.xnatCommandWrapperId = xnatCommandWrapperId;
+    public void setWrapperId(final long xnatCommandWrapperId) {
+        this.wrapperId = xnatCommandWrapperId;
     }
 
     public String getDockerImage() {
@@ -368,7 +368,7 @@ public class ContainerEntity extends AbstractHibernateEntity {
         return MoreObjects.toStringHelper(this)
                 .add("containerId", containerId)
                 .add("commandId", commandId)
-                .add("wrapperId", xnatCommandWrapperId)
+                .add("wrapperId", wrapperId)
                 .add("dockerImage", dockerImage)
                 .add("commandLine", commandLine)
                 .add("environmentVariables", environmentVariables)

src/main/java/org/nrg/containers/model/command/auto/Command.java
@@ -13,7 +13,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
-import org.nrg.containers.model.command.auto.Command.Builder;
 import org.nrg.containers.model.command.entity.CommandEntity;
 import org.nrg.containers.model.command.entity.CommandInputEntity;
 import org.nrg.containers.model.command.entity.CommandMountEntity;
@@ -25,7 +24,6 @@ import org.nrg.containers.model.command.entity.CommandWrapperInputType;
 import org.nrg.containers.model.command.entity.DockerCommandEntity;
 import org.nrg.containers.model.command.entity.CommandWrapperOutputEntity;
 import org.nrg.containers.model.command.entity.CommandWrapperEntity;
-import org.nrg.containers.model.configuration.CommandConfiguration;
 import org.nrg.containers.model.configuration.CommandConfiguration.CommandInputConfiguration;
 import org.nrg.containers.model.configuration.CommandConfiguration.CommandOutputConfiguration;
 
@@ -355,11 +353,11 @@ public abstract class Command {
                     wrapperInputNames.add(derived.name());
                 }
 
-                if (derived.name().equals(derived.derivedFromXnatInput())) {
+                if (derived.name().equals(derived.derivedFromWrapperInput())) {
                     errors.add(wrapperName + "derived input \"" + derived.name() + "\" is derived from itself.");
                 }
-                if (!wrapperInputNames.contains(derived.derivedFromXnatInput())) {
-                    errors.add(wrapperName + "derived input \"" + derived.name() + "\" is derived from an unknown XNAT input \"" + derived.derivedFromXnatInput() + "\". Known inputs: " + StringUtils.join(wrapperInputNames, ", "));
+                if (!wrapperInputNames.contains(derived.derivedFromWrapperInput())) {
+                    errors.add(wrapperName + "derived input \"" + derived.name() + "\" is derived from an unknown XNAT input \"" + derived.derivedFromWrapperInput() + "\". Known inputs: " + StringUtils.join(wrapperInputNames, ", "));
                 }
 
                 if (!inputErrors.isEmpty()) {
@@ -386,8 +384,8 @@ public abstract class Command {
                     handledOutputs.add(output.commandOutputName());
                 }
 
-                if (!wrapperInputNames.contains(output.xnatInputName())) {
-                    errors.add(wrapperName + "output handler refers to unknown XNAT input \"" + output.xnatInputName() + "\". Known inputs: " + knownWrapperInputs + ".");
+                if (!wrapperInputNames.contains(output.wrapperInputName())) {
+                    errors.add(wrapperName + "output handler refers to unknown XNAT input \"" + output.wrapperInputName() + "\". Known inputs: " + knownWrapperInputs + ".");
                 }
 
                 if (!outputErrors.isEmpty()) {
@@ -1056,14 +1054,14 @@ public abstract class Command {
     @AutoValue
     @JsonInclude(JsonInclude.Include.ALWAYS)
     public static abstract class CommandWrapperDerivedInput extends CommandWrapperInput {
-        @Nullable @JsonProperty("derived-from-xnat-input") public abstract String derivedFromXnatInput();
+        @Nullable @JsonProperty("derived-from-wrapper-input") public abstract String derivedFromWrapperInput();
         @Nullable @JsonProperty("derived-from-xnat-object-property") public abstract String derivedFromXnatObjectProperty();
 
         @JsonCreator
         static CommandWrapperDerivedInput create(@JsonProperty("name") final String name,
                                                  @JsonProperty("description") final String description,
                                                  @JsonProperty("type") final String type,
-                                                 @JsonProperty("derived-from-xnat-input") final String derivedFromXnatInput,
+                                                 @JsonProperty("derived-from-wrapper-input") final String derivedFromWrapperInput,
                                                  @JsonProperty("derived-from-xnat-object-property") final String derivedFromXnatObjectProperty,
                                                  @JsonProperty("matcher") final String matcher,
                                                  @JsonProperty("provides-value-for-command-input") final String providesValueForCommandInput,
@@ -1077,7 +1075,7 @@ public abstract class Command {
                     .name(name)
                     .description(description)
                     .type(type == null ? CommandWrapperDerivedInputEntity.DEFAULT_TYPE.getName() : type)
-                    .derivedFromXnatInput(derivedFromXnatInput)
+                    .derivedFromWrapperInput(derivedFromWrapperInput)
                     .derivedFromXnatObjectProperty(derivedFromXnatObjectProperty)
                     .matcher(matcher)
                     .providesValueForCommandInput(providesValueForCommandInput)
@@ -1100,7 +1098,7 @@ public abstract class Command {
                     .name(wrapperInput.getName())
                     .description(wrapperInput.getDescription())
                     .type(wrapperInput.getType().getName())
-                    .derivedFromXnatInput(wrapperInput.getDerivedFromXnatInput())
+                    .derivedFromWrapperInput(wrapperInput.getDerivedFromWrapperInput())
                     .derivedFromXnatObjectProperty(wrapperInput.getDerivedFromXnatObjectProperty())
                     .matcher(wrapperInput.getMatcher())
                     .providesValueForCommandInput(wrapperInput.getProvidesValueForCommandInput())
@@ -1127,7 +1125,7 @@ public abstract class Command {
                     .id(this.id())
                     .name(this.name())
                     .type(this.type())
-                    .derivedFromXnatInput(this.derivedFromXnatInput())
+                    .derivedFromWrapperInput(this.derivedFromWrapperInput())
                     .derivedFromXnatObjectProperty(this.derivedFromXnatObjectProperty())
                     .providesValueForCommandInput(this.providesValueForCommandInput())
                     .providesFilesForCommandMount(this.providesFilesForCommandMount())
@@ -1145,8 +1143,8 @@ public abstract class Command {
             // Derived inputs have all the same constraints as external inputs, plus more
             final List<String> errors = super.validate();
 
-            if (StringUtils.isBlank(derivedFromXnatInput())) {
-                errors.add("Command wrapper input \"" + name() + "\" - \"derived-from-xnat-input\" cannot be blank.");
+            if (StringUtils.isBlank(derivedFromWrapperInput())) {
+                errors.add("Command wrapper input \"" + name() + "\" - property \"derived-from-wrapper-input\" cannot be blank.");
             }
 
             return errors;
@@ -1166,7 +1164,7 @@ public abstract class Command {
             public abstract Builder rawReplacementKey(final String rawReplacementKey);
             public abstract Builder required(final boolean required);
             public abstract Builder loadChildren(final boolean loadChildren);
-            public abstract Builder derivedFromXnatInput(final String derivedFromXnatInput);
+            public abstract Builder derivedFromWrapperInput(final String derivedFromWrapperInput);
             public abstract Builder derivedFromXnatObjectProperty(final String derivedFromXnatObjectProperty);
 
             public abstract CommandWrapperDerivedInput build();
@@ -1179,21 +1177,21 @@ public abstract class Command {
         @JsonIgnore public abstract long id();
         @Nullable @JsonProperty("name") public abstract String name();
         @Nullable @JsonProperty("accepts-command-output") public abstract String commandOutputName();
-        @Nullable @JsonProperty("as-a-child-of-xnat-input") public abstract String xnatInputName();
+        @Nullable @JsonProperty("as-a-child-of-wrapper-input") public abstract String wrapperInputName();
         @JsonProperty("type") public abstract String type();
         @Nullable @JsonProperty("label") public abstract String label();
 
         @JsonCreator
         static CommandWrapperOutput create(@JsonProperty("name") final String name,
                                            @JsonProperty("accepts-command-output") final String commandOutputName,
-                                           @JsonProperty("as-a-child-of-xnat-input") final String xnatInputName,
+                                           @JsonProperty("as-a-child-of-wrapper-input") final String wrapperInputName,
                                            @JsonProperty("type") final String type,
                                            @JsonProperty("label") final String label) {
             return create(
                     0L,
                     name,
                     commandOutputName,
-                    xnatInputName,
+                    wrapperInputName,
                     type == null ? CommandWrapperOutputEntity.DEFAULT_TYPE.getName() : type,
                     label);
         }
@@ -1217,11 +1215,11 @@ public abstract class Command {
             if (wrapperOutput == null) {
                 return null;
             }
-            return create(wrapperOutput.getId(), wrapperOutput.getName(), wrapperOutput.getCommandOutputName(), wrapperOutput.getXnatInputName(), wrapperOutput.getType().getName(), wrapperOutput.getLabel());
+            return create(wrapperOutput.getId(), wrapperOutput.getName(), wrapperOutput.getCommandOutputName(), wrapperOutput.getWrapperInputName(), wrapperOutput.getType().getName(), wrapperOutput.getLabel());
         }
 
         public CommandWrapperOutput applyConfiguration(final CommandOutputConfiguration commandOutputConfiguration) {
-            return create(this.id(), this.name(), this.commandOutputName(), this.xnatInputName(), this.type(),
+            return create(this.id(), this.name(), this.commandOutputName(), this.wrapperInputName(), this.type(),
                     commandOutputConfiguration.label());
         }
 
@@ -1234,10 +1232,10 @@ public abstract class Command {
             }
 
             if (StringUtils.isBlank(commandOutputName())) {
-                errors.add("Command wrapper output \"" + name() + "\" - command output name cannot be blank.");
+                errors.add("Command wrapper output \"" + name() + "\" - property \"accepts-command-output\" cannot be blank.");
             }
-            if (StringUtils.isBlank(xnatInputName())) {
-                errors.add("Command wrapper output \"" + name() + "\" - xnat input name cannot be blank.");
+            if (StringUtils.isBlank(wrapperInputName())) {
+                errors.add("Command wrapper output \"" + name() + "\" - property \"as-a-child-of-wrapper-input\" cannot be blank.");
             }
             final List<String> types = CommandWrapperOutputEntity.Type.names();
             if (!types.contains(type())) {

src/main/java/org/nrg/containers/model/command/auto/ResolvedCommand.java
@@ -317,16 +317,16 @@ public abstract class ResolvedCommand {
     @AutoValue
     @JsonInclude(JsonInclude.Include.ALWAYS)
     public abstract static class ResolvedCommandMountFiles {
-        @JsonProperty("from-xnat-input") public abstract String fromXnatInput();
+        @JsonProperty("from-wrapper-input") public abstract String fromWrapperInput();
         @Nullable @JsonProperty("from-uri") public abstract String fromUri();
         @Nullable @JsonProperty("root-directory") public abstract String rootDirectory();
         @Nullable @JsonProperty("path") public abstract String path();
 
-        public static ResolvedCommandMountFiles create(@JsonProperty("from-xnat-input") final String fromXnatInput,
+        public static ResolvedCommandMountFiles create(@JsonProperty("from-wrapper-input") final String fromWrapperInput,
                                                        @JsonProperty("from-uri") final String fromUri,
                                                        @JsonProperty("root-directory") final String rootDirectory,
                                                        @JsonProperty("path") final String path) {
-            return new AutoValue_ResolvedCommand_ResolvedCommandMountFiles(fromXnatInput, fromUri, rootDirectory, path);
+            return new AutoValue_ResolvedCommand_ResolvedCommandMountFiles(fromWrapperInput, fromUri, rootDirectory, path);
         }
     }
 
@@ -340,7 +340,7 @@ public abstract class ResolvedCommand {
         @Nullable @JsonProperty("path") public abstract String path();
         @Nullable @JsonProperty("glob") public abstract String glob();
         @JsonProperty("label") public abstract String label();
-        @JsonProperty("handled-by-xnat-command-input") public abstract String handledByXnatCommandInput();
+        @JsonProperty("handled-by-wrapper-input") public abstract String handledByWrapperInput();
 
         @JsonCreator
         public static ResolvedCommandOutput create(@JsonProperty("name") final String name,
@@ -350,7 +350,7 @@ public abstract class ResolvedCommand {
                                                    @JsonProperty("path") final String path,
                                                    @JsonProperty("glob") final String glob,
                                                    @JsonProperty("label") final String label,
-                                                   @JsonProperty("handled-by-xnat-command-input") final String xnatInputName) {
+                                                   @JsonProperty("handled-by-wrapper-input") final String wrapperInputName) {
             return builder()
                     .name(name)
                     .type(type)
@@ -359,7 +359,7 @@ public abstract class ResolvedCommand {
                     .path(path)
                     .glob(glob)
                     .label(label)
-                    .handledByXnatCommandInput(xnatInputName)
+                    .handledByWrapperInput(wrapperInputName)
                     .build();
         }
 
@@ -376,7 +376,7 @@ public abstract class ResolvedCommand {
             public abstract Builder path(String path);
             public abstract Builder glob(String glob);
             public abstract Builder label(String label);
-            public abstract Builder handledByXnatCommandInput(String handledByXnatCommandInput);
+            public abstract Builder handledByWrapperInput(String handledByWrapperInput);
 
             public abstract ResolvedCommandOutput build();
         }

src/main/java/org/nrg/containers/model/command/entity/CommandWrapperDerivedInputEntity.java
@@ -37,12 +37,12 @@ public class CommandWrapperDerivedInputEntity {
     private boolean loadChildren = true;
     private String value;
 
-    @JsonProperty("derived-from-xnat-input") private String derivedFromXnatInput;
+    @JsonProperty("derived-from-wrapper-input") private String derivedFromWrapperInput;
     @JsonProperty("derived-from-xnat-object-property") private String derivedFromXnatObjectProperty;
 
     public static CommandWrapperDerivedInputEntity fromPojo(final Command.CommandWrapperDerivedInput commandWrapperInput) {
         final CommandWrapperDerivedInputEntity commandWrapperInputEntity = new CommandWrapperDerivedInputEntity();
-        commandWrapperInputEntity.derivedFromXnatInput = commandWrapperInput.derivedFromXnatInput();
+        commandWrapperInputEntity.derivedFromWrapperInput = commandWrapperInput.derivedFromWrapperInput();
         commandWrapperInputEntity.derivedFromXnatObjectProperty = commandWrapperInput.derivedFromXnatObjectProperty();
         commandWrapperInputEntity.id = commandWrapperInput.id();
         commandWrapperInputEntity.name = commandWrapperInput.name();
@@ -120,12 +120,12 @@ public class CommandWrapperDerivedInputEntity {
         this.commandWrapperEntity = commandWrapperEntity;
     }
 
-    public String getDerivedFromXnatInput() {
-        return derivedFromXnatInput;
+    public String getDerivedFromWrapperInput() {
+        return derivedFromWrapperInput;
     }
 
-    public void setDerivedFromXnatInput(final String derivedFromXnatInput) {
-        this.derivedFromXnatInput = derivedFromXnatInput;
+    public void setDerivedFromWrapperInput(final String derivedFromXnatInput) {
+        this.derivedFromWrapperInput = derivedFromXnatInput;
     }
 
     public String getDerivedFromXnatObjectProperty() {
@@ -267,7 +267,7 @@ public class CommandWrapperDerivedInputEntity {
                 .add("name", name)
                 .add("description", description)
                 .add("type", type)
-                .add("derivedFromXnatInput", derivedFromXnatInput)
+                .add("derivedFromWrapperInput", derivedFromWrapperInput)
                 .add("derivedFromXnatObjectProperty", derivedFromXnatObjectProperty)
                 .add("matcher", matcher)
                 .add("providesValueForCommandInput", providesValueForCommandInput)

src/main/java/org/nrg/containers/model/command/entity/CommandWrapperOutputEntity.java
@@ -29,7 +29,7 @@ public class CommandWrapperOutputEntity {
     @JsonIgnore private CommandWrapperEntity commandWrapperEntity;
     private String name;
     @JsonProperty("accepts-command-output") private String commandOutputName;
-    @JsonProperty("as-a-child-of-xnat-input") private String xnatInputName;
+    @JsonProperty("as-a-child-of-wrapper-input") private String wrapperInputName;
     private Type type;
     private String label;
 
@@ -38,7 +38,7 @@ public class CommandWrapperOutputEntity {
         commandWrapperOutputEntity.id = commandWrapperOutput.id();
         commandWrapperOutputEntity.name = commandWrapperOutput.name();
         commandWrapperOutputEntity.commandOutputName = commandWrapperOutput.commandOutputName();
-        commandWrapperOutputEntity.xnatInputName = commandWrapperOutput.xnatInputName();
+        commandWrapperOutputEntity.wrapperInputName = commandWrapperOutput.wrapperInputName();
         commandWrapperOutputEntity.label = commandWrapperOutput.label();
 
         switch (commandWrapperOutput.type()) {
@@ -98,12 +98,12 @@ public class CommandWrapperOutputEntity {
         this.commandOutputName = commandOutputName;
     }
 
-    public String getXnatInputName() {
-        return xnatInputName;
+    public String getWrapperInputName() {
+        return wrapperInputName;
     }
 
-    public void setXnatInputName(final String xnatInputName) {
-        this.xnatInputName = xnatInputName;
+    public void setWrapperInputName(final String xnatInputName) {
+        this.wrapperInputName = xnatInputName;
     }
 
     public String getLabel() {
@@ -134,7 +134,7 @@ public class CommandWrapperOutputEntity {
                 .add("id", id)
                 .add("name", name)
                 .add("commandOutputName", commandOutputName)
-                .add("xnatInputName", xnatInputName)
+                .add("wrapperInputName", wrapperInputName)
                 .add("type", type)
                 .add("label", label)
                 .toString();

src/main/java/org/nrg/containers/model/container/auto/Container.java
@@ -80,7 +80,7 @@ public abstract class Container {
         return builder()
                 .databaseId(containerEntity.getId())
                 .commandId(containerEntity.getCommandId())
-                .wrapperId(containerEntity.getXnatCommandWrapperId())
+                .wrapperId(containerEntity.getWrapperId())
                 .containerId(containerEntity.getContainerId())
                 .userId(containerEntity.getUserId())
                 .dockerImage(containerEntity.getDockerImage())

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityOutput.java
@@ -41,7 +41,7 @@ public class ContainerEntityOutput implements Serializable {
         this.glob = resolvedCommandOutput.glob();
         this.label = resolvedCommandOutput.label();
         this.type = resolvedCommandOutput.type();
-        this.handledByXnatCommandInput = resolvedCommandOutput.handledByXnatCommandInput();
+        this.handledByXnatCommandInput = resolvedCommandOutput.handledByWrapperInput();
     }
 
     public static ContainerEntityOutput fromPojo(final Container.ContainerOutput containerOutputPojo) {

src/main/java/org/nrg/containers/model/container/entity/ContainerMountFilesEntity.java
@@ -14,8 +14,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.util.Objects;
 
-import static org.nrg.containers.model.command.auto.Command.CommandWrapperInput;
-
 @Entity
 @Audited
 public class ContainerMountFilesEntity {
@@ -29,7 +27,7 @@ public class ContainerMountFilesEntity {
     public ContainerMountFilesEntity() {}
 
     public ContainerMountFilesEntity(final ResolvedCommandMountFiles resolvedCommandMountFiles) {
-        this.fromXnatInput = resolvedCommandMountFiles.fromXnatInput();
+        this.fromXnatInput = resolvedCommandMountFiles.fromWrapperInput();
         this.fromUri = resolvedCommandMountFiles.fromUri();
         this.rootDirectory = resolvedCommandMountFiles.rootDirectory();
         this.path = resolvedCommandMountFiles.path();
@@ -122,7 +120,7 @@ public class ContainerMountFilesEntity {
     public String toString() {
         return MoreObjects.toStringHelper(this)
                 .add("id", id)
-                .add("fromXnatInput", fromXnatInput)
+                .add("fromWrapperInput", fromXnatInput)
                 .add("fromUri", fromUri)
                 .add("rootDirectory", rootDirectory)
                 .add("path", path)

commit 988a5e836ba7e48164747b52f3b2db337c55ce85

Fix: wrapper database tables were broken after 7b9a9a28

src/main/java/org/nrg/containers/model/container/entity/ContainerEntity.java
@@ -140,8 +140,8 @@ public class ContainerEntity extends AbstractHibernateEntity {
         return wrapperId;
     }
 
-    public void setWrapperId(final long xnatCommandWrapperId) {
-        this.wrapperId = xnatCommandWrapperId;
+    public void setWrapperId(final long wrapperId) {
+        this.wrapperId = wrapperId;
     }
 
     public String getDockerImage() {