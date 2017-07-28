mohanakannan9/container-service

commit 3385972be091b4ebe2112135ccfb3c5f965dbd87

Add fromPojo creator methods to Container entities

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityMount.java
@@ -10,7 +10,9 @@ import org.hibernate.envers.Audited;
 import org.nrg.containers.model.command.auto.ResolvedCommand.PartiallyResolvedCommandMount;
 import org.nrg.containers.model.command.auto.ResolvedCommand.ResolvedCommandMount;
 import org.nrg.containers.model.command.auto.ResolvedCommand.ResolvedCommandMountFiles;
+import org.nrg.containers.model.container.auto.Container;
 
+import javax.annotation.Nullable;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -52,6 +54,24 @@ public class ContainerEntityMount implements Serializable {
                     }
                 })
         ));
+    }
+
+    public static ContainerEntityMount fromPojo(final Container.ContainerMount containerMountPojo) {
+        final ContainerEntityMount containerEntityMount = new ContainerEntityMount();
+        containerEntityMount.setId(containerMountPojo.databaseId());
+        containerEntityMount.setName(containerMountPojo.name());
+        containerEntityMount.setWritable(containerMountPojo.writable());
+        containerEntityMount.setXnatHostPath(containerMountPojo.xnatHostPath());
+        containerEntityMount.setContainerHostPath(containerMountPojo.containerHostPath());
+        containerEntityMount.setInputFiles(
+                Lists.newArrayList(Lists.transform(containerMountPojo.inputFiles(),
+                        new Function<Container.ContainerMountFiles, ContainerMountFilesEntity>() {
+                    @Override
+                    public ContainerMountFilesEntity apply(final Container.ContainerMountFiles input) {
+                        return ContainerMountFilesEntity.create(input);
+                    }
+                })));
+        return containerEntityMount;
     }
 
     @Id

src/main/java/org/nrg/containers/model/container/entity/ContainerEntity.java
@@ -11,6 +11,7 @@ import org.nrg.containers.model.command.auto.ResolvedCommand;
 import org.nrg.containers.model.command.auto.ResolvedCommand.ResolvedCommandMount;
 import org.nrg.containers.model.command.auto.ResolvedCommand.ResolvedCommandOutput;
 import org.nrg.containers.model.container.ContainerInputType;
+import org.nrg.containers.model.container.auto.Container;
 import org.nrg.framework.orm.hibernate.AbstractHibernateEntity;
 
 import javax.persistence.CascadeType;
@@ -72,6 +73,53 @@ public class ContainerEntity extends AbstractHibernateEntity {
                 })
         ));
         setLogPaths(null);
+    }
+
+    public static ContainerEntity fromPojo(final Container containerPojo) {
+        final ContainerEntity containerEntity = new ContainerEntity();
+        containerEntity.setId(containerPojo.databaseId());
+        containerEntity.setCommandId(containerPojo.commandId());
+        containerEntity.setXnatCommandWrapperId(containerPojo.wrapperId());
+        containerEntity.setContainerId(containerPojo.containerId());
+        containerEntity.setUserId(containerPojo.userId());
+        containerEntity.setDockerImage(containerPojo.dockerImage());
+        containerEntity.setCommandLine(containerPojo.commandLine());
+        containerEntity.setEnvironmentVariables(containerPojo.environmentVariables());
+        containerEntity.setLogPaths(containerPojo.logPaths());
+        containerEntity.setMounts(Lists.newArrayList(Lists.transform(
+                containerPojo.mounts(), new Function<Container.ContainerMount, ContainerEntityMount>() {
+                    @Override
+                    public ContainerEntityMount apply(final Container.ContainerMount input) {
+                        return ContainerEntityMount.fromPojo(input);
+                    }
+                }))
+        );
+        containerEntity.setInputs(Lists.newArrayList(Lists.transform(
+                containerPojo.inputs(), new Function<Container.ContainerInput, ContainerEntityInput>() {
+                    @Override
+                    public ContainerEntityInput apply(final Container.ContainerInput input) {
+                        return ContainerEntityInput.fromPojo(input);
+                    }
+                }))
+        );
+        containerEntity.setOutputs(Lists.newArrayList(Lists.transform(
+                containerPojo.outputs(), new Function<Container.ContainerOutput, ContainerEntityOutput>() {
+                    @Override
+                    public ContainerEntityOutput apply(final Container.ContainerOutput input) {
+                        return ContainerEntityOutput.fromPojo(input);
+                    }
+                }))
+        );
+        containerEntity.setHistory(Lists.newArrayList(Lists.transform(
+                containerPojo.history(), new Function<Container.ContainerHistory, ContainerEntityHistory>() {
+                    @Override
+                    public ContainerEntityHistory apply(final Container.ContainerHistory input) {
+                        return ContainerEntityHistory.fromPojo(input);
+                    }
+                }))
+        );
+
+        return containerEntity;
     }
 
     public long getCommandId() {

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityHistory.java
@@ -5,6 +5,7 @@ import com.google.common.base.MoreObjects;
 import org.hibernate.envers.Audited;
 import org.nrg.containers.events.model.ContainerEvent;
 import org.nrg.containers.events.model.DockerContainerEvent;
+import org.nrg.containers.model.container.auto.Container;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -57,6 +58,17 @@ public class ContainerEntityHistory {
         history.entityId = username;
         history.timeRecorded = new Date();
         return history;
+    }
+
+    public static ContainerEntityHistory fromPojo(final Container.ContainerHistory containerHistoryPojo) {
+        final ContainerEntityHistory containerEntityHistory = new ContainerEntityHistory();
+        containerEntityHistory.setId(containerHistoryPojo.databaseId());
+        containerEntityHistory.setStatus(containerHistoryPojo.status());
+        containerEntityHistory.setEntityType(containerHistoryPojo.entityType());
+        containerEntityHistory.setEntityId(containerHistoryPojo.entityId());
+        containerEntityHistory.setTimeRecorded(containerHistoryPojo.timeRecorded());
+        containerEntityHistory.setExternalTimestamp(containerHistoryPojo.externalTimestamp());
+        return containerEntityHistory;
     }
 
     @Id

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityInput.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
 import org.hibernate.envers.Audited;
 import org.nrg.containers.model.container.ContainerInputType;
+import org.nrg.containers.model.container.auto.Container;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -48,6 +49,15 @@ public class ContainerEntityInput {
         input.name = name;
         input.value = value;
         return input;
+    }
+
+    public static ContainerEntityInput fromPojo(final Container.ContainerInput containerInputPojo) {
+        final ContainerEntityInput containerEntityInput = new ContainerEntityInput();
+        containerEntityInput.setId(containerInputPojo.databaseId());
+        containerEntityInput.setType(containerInputPojo.type());
+        containerEntityInput.setName(containerInputPojo.name());
+        containerEntityInput.setValue(containerInputPojo.value());
+        return containerEntityInput;
     }
 
     @Id

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityOutput.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
 import org.hibernate.envers.Audited;
 import org.nrg.containers.model.command.auto.ResolvedCommand.ResolvedCommandOutput;
+import org.nrg.containers.model.container.auto.Container;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -41,6 +42,21 @@ public class ContainerEntityOutput implements Serializable {
         this.label = resolvedCommandOutput.label();
         this.type = resolvedCommandOutput.type();
         this.handledByXnatCommandInput = resolvedCommandOutput.handledByXnatCommandInput();
+    }
+
+    public static ContainerEntityOutput fromPojo(final Container.ContainerOutput containerOutputPojo) {
+        final ContainerEntityOutput containerEntityOutput = new ContainerEntityOutput();
+        containerEntityOutput.setId(containerOutputPojo.databaseId());
+        containerEntityOutput.setName(containerOutputPojo.name());
+        containerEntityOutput.setType(containerOutputPojo.type());
+        containerEntityOutput.setRequired(containerOutputPojo.required());
+        containerEntityOutput.setMount(containerOutputPojo.mount());
+        containerEntityOutput.setPath(containerOutputPojo.path());
+        containerEntityOutput.setGlob(containerOutputPojo.glob());
+        containerEntityOutput.setLabel(containerOutputPojo.label());
+        containerEntityOutput.setCreated(containerOutputPojo.created());
+        containerEntityOutput.setHandledByXnatCommandInput(containerOutputPojo.handledByWrapperInput());
+        return containerEntityOutput;
     }
 
     @Id

src/main/java/org/nrg/containers/model/container/entity/ContainerMountFilesEntity.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
 import org.hibernate.envers.Audited;
 import org.nrg.containers.model.command.auto.ResolvedCommand.ResolvedCommandMountFiles;
+import org.nrg.containers.model.container.auto.Container;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -32,6 +33,16 @@ public class ContainerMountFilesEntity {
         this.fromUri = resolvedCommandMountFiles.fromUri();
         this.rootDirectory = resolvedCommandMountFiles.rootDirectory();
         this.path = resolvedCommandMountFiles.path();
+    }
+
+    public static ContainerMountFilesEntity create(final Container.ContainerMountFiles containerMountFilesPojo) {
+        final ContainerMountFilesEntity containerMountFilesEntity = new ContainerMountFilesEntity();
+        containerMountFilesEntity.setId(containerMountFilesPojo.databaseId());
+        containerMountFilesEntity.setFromXnatInput(containerMountFilesPojo.fromXnatInput());
+        containerMountFilesEntity.setFromUri(containerMountFilesPojo.fromUri());
+        containerMountFilesEntity.setRootDirectory(containerMountFilesPojo.rootDirectory());
+        containerMountFilesEntity.setPath(containerMountFilesPojo.path());
+        return containerMountFilesEntity;
     }
 
     @Id

commit 0ceb543fb0a43fac6b8b6548d14f3563399c991b

Fix: could not read containers from db. Broken by 3385972b

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityMount.java
@@ -65,6 +65,7 @@ public class ContainerEntityMount implements Serializable {
         this.setWritable(containerMountPojo.writable());
         this.setXnatHostPath(containerMountPojo.xnatHostPath());
         this.setContainerHostPath(containerMountPojo.containerHostPath());
+        this.setContainerPath(containerMountPojo.containerPath());
         this.setInputFiles(
                 Lists.newArrayList(Lists.transform(containerMountPojo.inputFiles(),
                         new Function<Container.ContainerMountFiles, ContainerMountFilesEntity>() {